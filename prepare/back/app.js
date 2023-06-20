b08e4e9fee4f9b3efb6ef67fa3a1de72b666346c
areumYang <yangareum1818@nate.com>
2023-06-21 05:27:33 +0900

    router
    router분리하기
    /routes 폴더 추가, post.js 파일 추가
    
    /post url분리

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,15 +1,17 @@
 const express = require("express");
+const postRouter = require("./routes/post");
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("hello express");
 });
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.send("hello api");
 });
 
-app.get("/api/posts", (req, res) => {
+app.get("/posts", (req, res) => {
   res.json([
     { id: 1, content: "hello01" },
     { id: 2, content: "hello02" },
@@ -17,13 +19,7 @@ app.get("/api/posts", (req, res) => {
   ]);
 });
 
-app.post("/api/post", (req, res) => {
-  res.json({ id: 1, content: "hello" });
-});
-
-app.delete("/api/post", (req, res) => {
-  res.json({ id: 1 });
-});
+app.use("/post", postRouter);
 
 app.listen(3065, () => {
   console.log("서버 실행 중");
