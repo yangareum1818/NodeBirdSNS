d17c46cfa444a0ee3ebc7b26d8a7946a2efad7b2
areumYang <yangareum1818@nate.com>
2023-11-27 21:06:30 +0900

    redux-toolkit configureStore setting

--- prepare/front/pages/_app.js
+++ prepare/front/pages/_app.js
@@ -1,26 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
+import { Provider } from "react-redux";
 import wrapper from "../store/configureStore";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const NodeBird = ({ Component }) => {
+const NodeBird = ({ Component, ...rest }) => {
+  const { store, props } = wrapper.useWrappedStore(rest)
+  const { pageProps } = props;
+
+  console.log(props);
+  
   return (
-    <>
+    <Provider store={store}>
       <Head>
         <meta charSet="utf-8" />
         <link rel="shortcut icon" href="https://nodebird.com/favicon.ico" />
         <link rel="icon" href="https://nodebird.com/favicon.ico" />
         <title>NodeBird</title>
       </Head>
-      <Component />
-    </>
+      <Component pageProps={...pageProps} />
+    </Provider>
   );
 };
 
 NodeBird.propTypes = {
   Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.any.isRequired,
 };
 
-export default wrapper.withRedux(NodeBird);
+export function reportWebVitals(metric) {
+  console.log(metric);
+}
+
+export default NodeBird;
--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,36 +1,35 @@
+import { configureStore } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
-import { applyMiddleware, compose, legacy_createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import createSagaMiddleware from "redux-saga";
-
 import reducer from "../reducers";
-import rootSaga from "../sagas";
 
-const loggerMiddleware =
-  ({ dispatch, getState }) =>
-  (next) =>
-  (action) => {
-    // if (typeof action === `function`) {
-    //   return action(dispatch, getState, extraArgument);
-    // }
-    return next(action);
-  };
+// const loggerMiddleware =
+//   ({ dispatch, getState }) =>
+//   (next) =>
+//   (action) => {
+//     // if (typeof action === `function`) {
+//     //   return action(dispatch, getState, extraArgument);
+//     // }
+//     return next(action);
+//   };
 
-const configureStore = () => {
-  const sagaMiddleware = createSagaMiddleware();
-  const middleware = [sagaMiddleware, loggerMiddleware];
-  const enhancer =
-    process.env.NODE_ENV === "production"
-      ? compose(applyMiddleware(...middleware))
-      : composeWithDevTools(applyMiddleware(...middleware));
-  const store = legacy_createStore(reducer, enhancer);
-  store.sagaTask = sagaMiddleware.run(rootSaga);
+function getServerState() {
+  return typeof document !== "undefined"
+    ? JSON.parse(document.querySelector("#__NEXT_DATA__").textContent)?.props
+        .pageProps.initialState
+    : undefined;
+}
+const serverState = getServerState();
 
-  return store;
-};
+console.log("serverState", serverState);
 
-const wrapper = createWrapper(configureStore, {
+const makeStore = () =>
+  configureStore({
+    reducer,
+    devTools: true,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+    preloadedState: serverState,
+  });
+
+export default createWrapper(makeStore, {
   debug: process.env.NODE_ENV === "development",
 });
-
-export default wrapper;
