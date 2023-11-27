5513018575e7bb8e4d3f2e995c12d95671e0eb00
areumYang <yangareum1818@nate.com>
2023-11-27 23:21:45 +0900

    slice 추가 ing

--- prepare/front/reducers/user.js
+++ prepare/front/reducers/user.js
@@ -1,3 +1,5 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 import { produce } from "immer";
 
 export const initialState = {
@@ -38,52 +40,51 @@ export const initialState = {
   userInfo: null,
 };
 
-export const LOAD_USER_REQUEST = "LOAD_USER_REQUEST";
-export const LOAD_USER_SUCCESS = "LOAD_USER_SUCCESS";
-export const LOAD_USER_FAILURE = "LOAD_USER_FAILURE";
+export const logIn = createAsyncThunk("user/logIn", async (data) => {
+  const response = await axios.post("/user/login", data);
+  return response.data;
+});
 
-export const LOAD_MY_INFO_REQUEST = "LOAD_MY_INFO_REQUEST";
-export const LOAD_MY_INFO_SUCCESS = "LOAD_MY_INFO_SUCCESS";
-export const LOAD_MY_INFO_FAILURE = "LOAD_MY_INFO_FAILURE";
+export const logOut = createAsyncThunk("user/logOut", async () => {
+  const response = await axios.post("/user/logout");
+  return response.data;
+});
 
-export const LOG_IN_REQUEST = "LOG_IN_REQUEST";
-export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS";
-export const LOG_IN_FAILURE = "LOG_IN_FAILURE";
-
-export const LOG_OUT_REQUEST = "LOG_OUT_REQUEST";
-export const LOG_OUT_SUCCESS = "LOG_OUT_SUCCESS";
-export const LOG_OUT_FAILURE = "LOG_OUT_FAILURE";
-
-export const SIGN_UP_REQUEST = "SIGN_UP_REQUEST";
-export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
-export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE";
-
-export const CHANGE_NICKNAME_REQUEST = "CHANGE_NICKNAME_REQUEST";
-export const CHANGE_NICKNAME_SUCCESS = "CHANGE_NICKNAME_SUCCESS";
-export const CHANGE_NICKNAME_FAILURE = "CHANGE_NICKNAME_FAILURE";
-
-export const FOLLOW_REQUEST = "FOLLOW_REQUEST";
-export const FOLLOW_SUCCESS = "FOLLOW_SUCCESS";
-export const FOLLOW_FAILURE = "FOLLOW_FAILURE";
-
-export const UNFOLLOW_REQUEST = "UNFOLLOW_REQUEST";
-export const UNFOLLOW_SUCCESS = "UNFOLLOW_SUCCESS";
-export const UNFOLLOW_FAILURE = "UNFOLLOW_FAILURE";
-
-export const LOAD_FOLLOWERS_REQUEST = "LOAD_FOLLOWERS_REQUEST";
-export const LOAD_FOLLOWERS_SUCCESS = "LOAD_FOLLOWERS_SUCCESS";
-export const LOAD_FOLLOWERS_FAILURE = "LOAD_FOLLOWERS_FAILURE";
-
-export const LOAD_FOLLOWINGS_REQUEST = "LOAD_FOLLOWINGS_REQUEST";
-export const LOAD_FOLLOWINGS_SUCCESS = "LOAD_FOLLOWINGS_SUCCESS";
-export const LOAD_FOLLOWINGS_FAILURE = "LOAD_FOLLOWINGS_FAILURE";
-
-export const REMOVE_FOLLOWER_REQUEST = "REMOVE_FOLLOWER_REQUEST";
-export const REMOVE_FOLLOWER_SUCCESS = "REMOVE_FOLLOWER_SUCCESS";
-export const REMOVE_FOLLOWER_FAILURE = "REMOVE_FOLLOWER_FAILURE";
-
-export const ADD_POST_TO_ME = "ADD_POST_TO_ME";
-export const REMOVE_POST_OF_ME = "REMOVE_POST_OF_ME";
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) =>
+    builder
+      .addCase(logIn.pending, (state) => {
+        state.logInLoading = true;
+        state.logInError = false;
+        state.logInDone = false;
+      })
+      .addCase(logIn.fulfilled, (state, action) => {
+        state.logInLoading = false;
+        state.me = action.payload;
+        state.logInDone = true;
+      })
+      .addCase(logIn.rejected, (state, action) => {
+        state.logInLoading = false;
+        state.logInError = action.error.message;
+      })
+      .addCase(logOut.pending, (state) => {
+        state.logOutLoading = true;
+        state.logOutDone = false;
+        state.logOutError = false;
+      })
+      .addCase(logOut.fulfilled, (state, action) => {
+        state.logOutLoading = false;
+        state.me = null;
+        state.logOutDone = true;
+      })
+      .addCase(logOut.rejected, (state, action) => {
+        state.logOutLoading = false;
+        state.logOutError = action.error.message;
+      }),
+});
 
 export const loginRequestAction = (data) => {
   return {
@@ -291,4 +292,4 @@ const reducer = (state = initialState, action) => {
     }
   });
 };
-export default reducer;
+export default userSlice;
