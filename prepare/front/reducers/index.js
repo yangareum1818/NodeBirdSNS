33f212fb3e262d14ef2871b7021ea106298c7ba2
areumYang <yangareum1818@nate.com>
2023-11-28 20:29:38 +0900

    feat : lodash + post reducer redux-toolkit setting

--- prepare/front/reducers/index.js
+++ prepare/front/reducers/index.js
@@ -4,12 +4,14 @@ import post from "./post";
 import { combineReducers } from "redux";
 import axios from "axios";
 import userSlice from "./user";
+import postSlice from "./post";
 
 axios.defaults.baseURL = "http://localhost:3065";
 axios.defaults.withCredentials = true;
 
 const rootReducer = combineReducers({
   user: userSlice.reducer,
+  post: postSlice.reducer,
 });
 
 // const initialState = {
